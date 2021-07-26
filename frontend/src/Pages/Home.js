45e99e25815e0d14590486b9e8f866da83a286d5
Joseph Ifergan <joseph@maison.fr>
2021-07-26 12:00:03 +0200

    Redux working in Product-Page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import { Col, Row } from 'react-bootstrap';
 import Product from "../Components/Product";
+import Loader from "../Components/Loader";
+import Message from "../Components/Message";
 import { listProducts } from "../actions/productActions";
 
 const Home = () => {
@@ -17,15 +19,15 @@ const Home = () => {
     return (
         <>
             <h1>Nos produits</h1>
-            {loading ? (<h2>Chargement...</h2>)
-            : error ? (<h3>{error}</h3>)
-            : <Row>
+            {loading ? (<Loader/>)
+            : error ? (<Message variant="danger">{error}</Message>)
+            : (<Row>
                         {products.map(product => (
                             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                 <Product product={product}/>
                             </Col>
                         ))}
-              </Row>
+              </Row>)
             }
         </>
     );
--- frontend/src/Pages/Product-Page.js
+++ frontend/src/Pages/Product-Page.js
@@ -1,74 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 import { Row, Col, Button, Card, Image, ListGroup } from "react-bootstrap";
 import Rating from "../Components/Rating";
-import axios from "axios";
+import { listProductsDetails } from "../actions/productActions";
+import Loader from "../Components/Loader";
+import Message from "../Components/Message";
 
 
 const ProductPage = ({ match }) => {
-
-    const [product, updateProduct] = useState({});
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product } = productDetails
 
     useEffect(() => {
-        const getProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`)
-            updateProduct(data)
-        }
-        getProduct()
-    }, [match])
+        dispatch(listProductsDetails(match.params.id))
+
+    }, [dispatch, match])
+
+
     return (
         <>
             <Link className="btn btn-info rounded my-3" to="/">Retour</Link>
-                <Row>
-                    <Col md={6}>
-                        <Image src={product.image} alt={product.name} fluid/>
-                    </Col>
-                    <Col md={3}>
-                        <ListGroup>
-                            <ListGroup.Item>
-                                <h2 className="product_title">{ product.name }</h2>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Rating
-                                    value={product.rating}
-                                    text={`${product.numReviews} avis`}
-                                />
-                            </ListGroup.Item>
-                            <ListGroup.Item><h1>{product.price} €</h1></ListGroup.Item>
-                            <ListGroup.Item>{product.description}</ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button className="add-to-card btn btn-block rounded"
-                                        type="button"
-                                        disabled={product.countInStock === 0}
-                                >
-                                    Ajouter au panier</Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Col>
-                    <Col md={3}>
-                        <Card>
-                            <ListGroup>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Price:</Col>
-                                        <Col>
-                                            <strong>{product.price} €</strong>
-                                        </Col>
-                                    </Row>
-                                </ListGroup.Item>
+            {loading ? (<Loader/>)
+            : error ? (<Message variant="danger">{error}</Message>)
+            : (<Row>
+                            <Col md={6}>
+                                <Image src={product.image} alt={product.name} fluid/>
+                            </Col>
+                            <Col md={3}>
+                                <ListGroup>
+                                    <ListGroup.Item>
+                                        <h2 className="product_title">{ product.name }</h2>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Rating
+                                            value={product.rating}
+                                            text={`${product.numReviews} avis`}
+                                        />
+                                    </ListGroup.Item>
+                                    <ListGroup.Item><h1>{product.price} €</h1></ListGroup.Item>
+                                    <ListGroup.Item>{product.description}</ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Button className="add-to-card btn btn-block rounded"
+                                                type="button"
+                                                disabled={product.countInStock === 0}
+                                        >
+                                            Ajouter au panier</Button>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                            <Col md={3}>
+                                <Card>
+                                    <ListGroup>
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Price:</Col>
+                                                <Col>
+                                                    <strong>{product.price} €</strong>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
 
-                                <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        {product.countInStock > 0 ? "En stock" : "Article indisponible"}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>
+                                                    {product.countInStock > 0 ? "En stock" : "Article indisponible"}
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Card>
 
-                    </Col>
-                </Row>
+                            </Col>
+                        </Row>
+                    )
+            }
+
         </>
     );
 };
