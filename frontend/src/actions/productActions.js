764fb26149f52a683c144c919743d29cc380fe8b
Joseph Ifergan <joseph@maison.fr>
2021-07-26 09:48:30 +0200

    Setting Redux Store, Reducers and Actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { productListReducer } from "../reducers/ProductsReducers";
+
+
+export const listProducts = () => async (dispatch) => {
+    try {
+        dispatch({type: PRODUCT_LIST_REQUEST })
+
+        const { data } = await axios.get('/api/products')
+
+        dispatch({
+            type: 'PRODUCT_LIST_SUCCESS',
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: 'PRODUCT_LIST_FAIL',
+            payload: error.message
+        })
+    }
+
+}
