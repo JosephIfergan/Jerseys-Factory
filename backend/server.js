e7f147d279720a282512a8e218636ff46bb28323
Joseph Ifergan <joseph@maison.fr>
2021-08-19 14:21:02 +0200

    Add Paypal Payment feature in Order.js

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,9 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
+app.get('/api/config/paypal', (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Le serveur fonctionne en mode ${process.env.NODE_ENV} sur le port ${PORT}`));
