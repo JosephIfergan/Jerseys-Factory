0951f3a630faef49921c0ef2a925c8ddf8f7e28b
Joseph Ifergan <joseph@maison.fr>
2021-08-24 10:02:44 +0200

    Add errorMiddleware in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -27,3 +28,5 @@ app.get('/api/config/paypal', (req, res) =>
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Le serveur fonctionne en mode ${process.env.NODE_ENV} sur le port ${PORT}`));
+
+app.use(errorHandler)
