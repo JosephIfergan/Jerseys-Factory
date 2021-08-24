cb9129b0d2c46ce4e71b2947104d8e1a50bd932d
Joseph Ifergan <joseph@maison.fr>
2021-08-24 16:19:39 +0200

    Add list of products when Admin logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Container } from 'react-bootstrap';
 import Home from "./Pages/Home"
-import NotFound from "./Pages/NotFound";
+// import NotFound from "./Pages/NotFound";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Profile from "./Pages/Profile";
@@ -16,6 +16,7 @@ import PlaceOrder from "./Pages/PlaceOrder";
 import Order from "./Pages/Order";
 import UserList from "./Pages/UserList";
 import UserEdit from "./Pages/UserEdit";
+import ProductList from "./Pages/ProductList";
 
 
 
@@ -27,7 +28,7 @@ const App = () => {
                 <Container>
                     <Switch>
                     <Route path="/" exact component={Home}/>
-                    <Route path='/404' component={NotFound} />
+                    {/*<Route path='/404' component={NotFound} />*/}
                     <Route path="/login" exact component={Login}/>
                     <Route path="/register" exact component={Register}/>
                     <Route path="/profile" exact component={Profile}/>
@@ -39,8 +40,9 @@ const App = () => {
                     <Route path="/cart/:id?" component={Cart}/>
                     <Route path="/admin/userlist" component={UserList}/>
                     <Route path="/admin/user/:id/edit" component={UserEdit}/>
+                    <Route path="/admin/productlist" component={ProductList}/>
                     {/*Le "?" permet d'y accéder même si le panier est vide et donc qu'il n'y pas d'ID */}
-                    <Redirect to="/404" />
+                    {/*<Redirect to="/404" />*/}
                     </Switch>
                 </Container>
             </main>
